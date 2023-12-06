5fc41b3d24d79811964dfe97c9800172ac576753
Vikash Rai <vikashrai2507@gmail.com>
2023-12-06 23:21:12 +0530

    FirstCommit for developer efficiency

---
+++ src/main/java/com/project/MetricLens/MetricLensApplication.java
@@ -0,0 +1,13 @@
+package com.project.MetricLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricLensApplication.class, args);
+	}
+
+}
