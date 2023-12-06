abae88285afabe5553f8bdf0b5a5bbb1c04f95d4
Vikash Rai <vikashrai2507@gmail.com>
2023-12-06 23:50:14 +0530

    Short Project Description added in application

--- src/main/java/com/project/MetricLens/MetricLensApplication.java
+++ src/main/java/com/project/MetricLens/MetricLensApplication.java
@@ -5,9 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MetricLensApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MetricLensApplication.class, args);
-	}
+    /**
+     * An springboot application [MetricLens]
+     * for measuring developer efficiency through gitlab,jenkins,jira
+     * using grafana as visualization tool and influxdb or mongodb or mysql as datasource.
+     * Author: Vikash Rai
+     * vikashrai2507@gmail.com
+     * vikashrai.in@gmail.com
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MetricLensApplication.class, args);
+    }
 
 }
